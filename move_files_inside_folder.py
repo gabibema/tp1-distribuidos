617bf35d9fcb1e74b2782f8f851514731e8a64d7
Gabriel Bedoya <gabriel.bedoya.m@gmail.com>
2024-04-28 21:42:47 -0300

    move script inside folder script

--- move_files_inside_folder.py
+++ move_files_inside_folder.py
@@ -1,26 +1,29 @@
 import os
 import shutil
 
-def move_and_rename_files(directory):
-    """Move each .py file in the specified directory into a subdirectory named after the file and rename it to main.py."""
-    for filename in os.listdir(directory):
+def move_and_rename_files(directory, mapping):
+    """Move each .py file in the specified directory into a subdirectory based on a mapping or named after the file, then rename it to main.py."""
 
+    for filename in os.listdir(directory):
         if filename.endswith('.py'):
-            subdir_name = os.path.splitext(filename)[0]
+            file_base_name = os.path.splitext(filename)[0]
+            
+            subdir_name = mapping.get(file_base_name, file_base_name)
             subdir_path = os.path.join(directory, subdir_name)
             
-            # Crear el subdirectorio si no existe
             os.makedirs(subdir_path, exist_ok=True)
-            
 
             original_file_path = os.path.join(directory, filename)
-            new_file_path = os.path.join(subdir_path, 'test.py')
+            new_file_path = os.path.join(subdir_path, 'main.py')
             
-            # Mover y renombrar el archivo al subdirectorio
             shutil.move(original_file_path, new_file_path)
             print(f"Moved {filename} to {new_file_path}")
 
 if __name__ == "__main__":
-
     directory_to_process = './workers'
-    move_and_rename_files(directory_to_process)
+    file_to_subdir_mapping = {
+        'top_10': 'top_fiction_books',
+        'nlp': 'nlp_worker',
+        'title_filter': 'computer_books'
+    }
+    move_and_rename_files(directory_to_process, file_to_subdir_mapping)
