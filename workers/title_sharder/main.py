fcc400f1a7138492236a544e88e90d133191f470
Manu Diaz <jumdiaz@fi.uba.ar>
2024-05-01 20:02:55 -0300

    fix routing issue in title sharder

--- workers/title_sharder/main.py
+++ workers/title_sharder/main.py
@@ -7,7 +7,7 @@ def routing_fn(msg):
         return [f"reviews_shard{shard_id}_{msg['request_id']}" for shard_id in range(SHARD_COUNT)]
     else:
         shard_id = hash(msg['Title']) % SHARD_COUNT
-        return [f"reviews_shard{shard_id}_{msg['request_id']}"]
+        return f"reviews_shard{shard_id}_{msg['request_id']}"
 
 def main():
     # Pending: move variables to env.
@@ -15,7 +15,9 @@ def main():
     rabbit_hostname = 'rabbitmq'
     src_queue = 'reviews_queue'
     dst_exchange = 'reviews_sharded_exchange'
-    tmp_queues = [('fiction_reviews', 'reviews'),('90s_reviews','reviews')]
+    fiction_tmp_queues = [(f'fiction_reviews_shard{shard_id}', f'reviews_shard{shard_id}') for shard_id in range(SHARD_COUNT)]
+    nineties_tmp_queues = [(f'90s_reviews_shard{shard_id}', f'reviews_shard{shard_id}') for shard_id in range(SHARD_COUNT)]
+    tmp_queues = fiction_tmp_queues + nineties_tmp_queues
     worker = DynamicRouter(routing_fn, tmp_queues, rabbit_hostname, src_queue, dst_exchange=dst_exchange)
     worker.start()
 
