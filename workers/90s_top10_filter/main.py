1848cd4cc22feadb49c19771c407a50f48b7c202
Manu Diaz <jumdiaz@fi.uba.ar>
2024-05-03 01:41:20 -0300

    fix bug where equal review count lead to dict comparison

--- workers/90s_top10_filter/main.py
+++ workers/90s_top10_filter/main.py
@@ -6,12 +6,11 @@ def aggregate(msg, accumulator):
     if type(msg) != list:
         msg = [msg]
     accumulator[msg[0]['request_id']] = accumulator.get(msg[0]['request_id'], [])
-    for book in msg:
-        accumulator[book['request_id']].append( (book['count'], book) )
+    accumulator[book['request_id']].extend(msg)
 
 def result(msg, accumulator):
     request_titles = accumulator.pop(msg['request_id'], [])
-    return json.dumps({'request_id': msg['request_id'], 'top10': [msg[1] for msg in nlargest(10, request_titles)]})
+    return json.dumps({'request_id': msg['request_id'], 'top10': [book for book in nlargest(10, request_titles, key=lambda title: title['count'])]})
 
 def main():
     # Pending: move variables to env.
