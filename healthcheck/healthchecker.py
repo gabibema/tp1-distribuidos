87d51aa6ad511d01ce848930ee2b94c54c83ac0a
Gabriel Bedoya <gabriel.bedoya.m@gmail.com>
2024-06-21 12:42:57 -0300

    working bully leader algorithm

--- healthcheck/healthchecker.py
+++ healthcheck/healthchecker.py
@@ -5,51 +5,46 @@ from lib.broker import MessageBroker
 from pika.exchange_type import ExchangeType
 import logging
 
+LEADER_ID = 1000000
+
 class PeerStorage:
     def __init__(self):
         self.manager = Manager()
-        self.peers = self.manager.list()
-        self.event = self.manager.Event()
+        self.peers = self.manager.dict()
 
     def add_peer(self, peer_id):
-        self.peers.append(peer_id)
-        self.event.set()  # Activar el evento cuando se añade un peer
+        self.peers[peer_id] = True
 
     def peer_exists(self, peer_id):
-        return peer_id in self.peers
+        return peer_id in self.get_peers()
 
     def get_peers(self):
-        return self.peers
+        return [peer for peer in self.peers.keys() if self.peers[peer]]
 
     def delete_peers(self):
-        self.peers = self.manager.list()  # Reemplazar con una nueva lista
-
-    def wait_for_peers(self):
-        self.event.wait()  # Esperar hasta que se active el evento
-        self.event.clear()  # Limpiar el evento para futuras esperas
+        for peer in self.peers.keys():
+            self.peers[peer] = False
 
 
-class HealthChecker:
+class Bully:
     def __init__(self, config):
+        self.manager = Manager()
         self.health = True
-        self.id = config['id']
+        self.id = self.manager.Value('i', config['id'])
         self.hostname = config['hostname']
         self.stored_peers = PeerStorage()
         self.receiver = Process(target=self.__receive, args=(self.stored_peers,))
         self.sender = Process(target=self.__send, args=(self.stored_peers,))
-        self.gather = Process(target=self.__gather, args=(self.stored_peers,))
 
     def start(self):
         try: 
             self.receiver.start()
             self.sender.start()
-            self.gather.start()
         except Exception as e:
             logging.error(f"Error starting health checker: {e}")
         finally:
             self.receiver.join()
             self.sender.join()
-            self.gather.join()
 
     def __init_connection(self, stored_peers):
         connection = MessageBroker(self.hostname)
@@ -74,35 +69,37 @@ class HealthChecker:
         connection.create_router("health_bully", ExchangeType.fanout)
         try:
             while True:
-                logging.warning(f"Already in stored_peers: {list(self.stored_peers.get_peers())}")
-                connection.send_message("health_bully", "", str(self.id))
-                logging.warning(f"Sent {self.id}") 
+                #logging.warning(f"Already in stored_peers: {list(self.stored_peers.get_peers())}")
+                connection.send_message("health_bully", "", str(self.id.value))
+                #logging.warning(f"Sent {self.id.value}") 
                 sleep(5)
                 peers = list(self.stored_peers.get_peers())
-                logging.warning(f"Peers: {peers}")
-                if len(peers) and self.id == max(peers):
-                    logging.warning(f"Health i'm the bully")
+                #logging.warning(f"Peers: {peers}")
+                if len(peers) and self.id.value == max(peers):
+                    #logging.warning(f"I'm the bully: {self.id.value}")
+                    self.id.value = LEADER_ID
                 
                 self.stored_peers.delete_peers()
-                logging.warning(f"Deleted peers: {list(self.stored_peers.get_peers())}")
                 sleep(30)
-                
-
         finally:
             connection.close_connection()
 
-    def __gather(self, stored_peers: PeerStorage):
-        pass
-
     def __callback(self, ch, method, properties, body, connection):
-        logging.warning(f"Received {body}")
-        if not self.stored_peers.peer_exists(self.id):
-            logging.warning(f"Sending {self.id} because {body} is not in stored_peers")
-            connection.send_message("health_bully", "", str(self.id))
-            self.stored_peers.add_peer(self.id)
+        if not self.stored_peers.peer_exists(self.id.value):
+            #logging.warning(f"Sending {self.id.value} because i'm not in stored_peers")
+            connection.send_message("health_bully", "", str(self.id.value))
+            self.stored_peers.add_peer(self.id.value)
         body = int(body)
         if not self.stored_peers.peer_exists(body):
-            logging.warning(f"Adding {body}")
+            #logging.warning(f"Adding {body}")
             self.stored_peers.add_peer(body)
             
         ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
+class HealthChecker:
+    def __init__(self, config):
+        self.bully = Bully(config)
+
+    def start(self):
+        self.bully.start()
