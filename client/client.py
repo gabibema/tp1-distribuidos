35f0026fba6d4bbe59e815b484584fad51632f2d
Manu Diaz <jumdiaz@fi.uba.ar>
2024-06-27 16:46:56 -0300

    fix bug where last non-EOF message used duplicate ID

--- client/client.py
+++ client/client.py
@@ -104,7 +104,7 @@ class Client:
             start_id = self.checkpoint[source]["message_id"]
             eof = self.checkpoint[source]["eof"]
         if eof:
-            #logging.warning(f'Skipping file {path} due to EOF')
+            logging.warning(f'Skipping file {path} due to EOF')
             return
 
         with open(path, READ_MODE) as file:
@@ -129,6 +129,7 @@ class Client:
 
         if len(batch) > 1:
             batch[-1] = batch[-1].rstrip()
+            message_id += 1
             queue.put((message_id, ''.join(batch)))
 
         batch = ['type', 'EOF']
