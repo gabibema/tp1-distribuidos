a34130df47954bf9890d68c1a2ead918a1ea44e0
Manu Diaz <mdiaz@edrans.com>
2024-06-29 13:20:47 -0300

    fix YA bug in percentile calculator

--- workers/fiction_percentile_calculator/main.py
+++ workers/fiction_percentile_calculator/main.py
@@ -11,16 +11,21 @@ def aggregate(msg, accumulator):
 
 def result(msg, accumulator):
     acc = accumulator.pop(msg['request_id'], [])
-    percentile_10_idx = len(acc) / 10
+    percentile_10_idx = len(acc) // 10
     if len(acc) <= 0:
+        logging.warning(f"Received EOF for {msg['request_id']} without any books.")
         return json.dumps([{'request_id': msg['request_id'], 'percentile': float('inf')}])
+    if percentile_10_idx == 0:
+        logging.warning(f"Received EOF for {msg['request_id']} with less than 10 books.")
+        return json.dumps([{'request_id': msg['request_id'], 'percentile': float('-inf')}])
     percentile = kth_smallest(percentile_10_idx, acc, 0, len(acc) - 1)
-    logging.warning(f'Received {len(acc)} titles for {msg["request_id"]}. The 10th percentile is {percentile}')
+    logging.warning(f'The 10th percentile is {percentile}.')
     return json.dumps([{'request_id': msg['request_id'], 'percentile': percentile}])
 
 def kth_smallest(k, buffer, start, end):
     "Find the Kth smallest value in O(n) time"
     i, j = start, end
+    logging.info(f'k: {k}, start: {start}, end: {end}.')
     pivot = choose_pivot(buffer, i, j)
     pivot_duplicates = 0
     # Create (in-place) 2 sub buffers. One with elements lesser than the pivot and another one with elements greater than the pivot.
@@ -37,17 +42,19 @@ def kth_smallest(k, buffer, start, end):
     # Determine which sub buffer contains the Kth element and run recursively
     lesser_buffer_size = i - start
     greater_buffer_size = end - j
+    logging.debug(f'i: {i}, j: {j}, pivot_duplicates: {pivot_duplicates}.')
     if k <= lesser_buffer_size:
         return kth_smallest(k, buffer, start, i - 1)
-    if len(buffer) - k < greater_buffer_size:
-        return kth_smallest(k, buffer, j + 1, end)
+    len_buffer = end - start + 1
+    if len_buffer - k < greater_buffer_size:
+        return kth_smallest(k - (j + 1 - start), buffer, j + 1, end)
     return pivot
 
 def choose_pivot(buffer, i, j):
     "Find 3 random posible pivots, and choose the one with the intermediate value"
-    pivot_1 = buffer[random.randrange(i,j+1)]
-    pivot_2 = buffer[random.randrange(i,j+1)]
-    pivot_3 = buffer[random.randrange(i,j+1)]
+    pivot_1 = buffer[random.randint(i,j)]
+    pivot_2 = buffer[random.randint(i,j)]
+    pivot_3 = buffer[random.randint(i,j)]
     if pivot_1 <= pivot_2 <= pivot_3:
         return pivot_2
     if pivot_1 <= pivot_3 <= pivot_2:
